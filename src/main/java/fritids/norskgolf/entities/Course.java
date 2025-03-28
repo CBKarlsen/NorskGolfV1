089e3d052f20bd0b46c76a02da79c883b8835a12
Casper Karlsen <casperbkarlsen@gmail.com>
2025-03-28 11:43:00 +0100

    Added setters and getters

--- src/main/java/fritids/norskgolf/entities/Course.java
+++ src/main/java/fritids/norskgolf/entities/Course.java
@@ -15,6 +15,46 @@ public class Course {
     @Column(nullable = false)
     private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
     private Double latitude;
     private Double longitude;
     private String externalId; // Unique identifier from a map API (recommended)
@@ -32,7 +72,6 @@ public class Course {
         this.players = players;
     }
 
-    // ... other getters and setters
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/fritids/norskgolf/entities/User.java
+++ src/main/java/fritids/norskgolf/entities/User.java
@@ -26,7 +26,6 @@ public class User {
     )
     private Set<Course> playedCourses = new HashSet<>();
 
-    // Constructors, getters, setters, etc.
 
     public void addPlayedCourse(Course course) {
         this.playedCourses.add(course);
@@ -44,5 +43,27 @@ public class User {
         this.playedCourses = playedCourses;
     }
 
-    // ... other getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
