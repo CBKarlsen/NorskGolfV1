b2aa1dcd700d8be9bf1942b175dcab7949c69e80
Casper Karlsen <casperbkarlsen@gmail.com>
2026-01-18 20:53:40 +0100

    redirect controller not working

--- backend/src/main/java/fritids/norskgolf/controller/SpaRedirectController.java
+++ backend/src/main/java/fritids/norskgolf/controller/SpaRedirectController.java
@@ -1,4 +1,4 @@
-package fritids.norskgolf;
+package fritids.norskgolf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
